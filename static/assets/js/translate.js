189bad51eabcfc4d35d4fba9123e3462b9e4ba1c
wendellast <wendelalves01a@hotmail.com>
2024-07-20 23:22:06 -0300

    css site: created

---
+++ static/assets/js/translate.js
@@ -0,0 +1,21 @@
+const inputBox = document.getElementById('inputBox');
+const outputBox = document.getElementById('outputBox');
+const copyButton = document.getElementById('copyButton');
+
+inputBox.addEventListener('input', () => {
+    outputBox.innerHTML = inputBox.innerHTML;
+});
+
+copyButton.addEventListener('click', () => {
+    const range = document.createRange();
+    range.selectNode(outputBox);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    try {
+        document.execCommand('copy');
+        alert('Texto copiado para a área de transferência!');
+    } catch (err) {
+        alert('Erro ao copiar texto');
+    }
+    window.getSelection().removeAllRanges();
+});
